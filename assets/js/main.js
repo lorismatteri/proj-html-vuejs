24963dde10f49ee5572ff52f6dac06b7fe375952
Loris Matteri <loris.matteri93@gmail.com>
2021-01-19 18:20:35 +0100

    Prima navbar ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,35 @@ let app = new Vue ({
     el: '#app',
     data: {
         logo: './assets/img/logo.png',
+        socials: [
+            {
+                icon: 'fas fa-phone-alt',
+                text: '+1 (305) 1234-5678',
+            },
+            {
+                icon: 'fas fa-envelope',
+                text: 'hello@example.com',
+            },
+            {
+                icon: 'fab fa-facebook-f',
+                text: '',
+            },
+            {
+                icon: 'fab fa-twitter',
+                text: '',
+            },
+            {
+                icon: 'fab fa-linkedin-in',
+                text: '',
+            },         
+        ]
     },
     methods: {
 
     }
 })
+
+
+
+
+
