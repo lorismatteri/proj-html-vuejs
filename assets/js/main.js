5b85d47aa7ce80bb2609ed028867b781c4f73b00
Loris Matteri <loris.matteri93@gmail.com>
2021-01-20 10:27:49 +0100

    Navbar principale ok
    manca da sistemare estetica

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,7 @@ let app = new Vue ({
     data: {
         logo: './assets/img/logo.png',
         socials: [
+            //per creare lista a destra nella prima nav dell'header
             {
                 icon: 'fas fa-phone-alt',
                 text: '+1 (305) 1234-5678',
@@ -23,6 +24,29 @@ let app = new Vue ({
                 icon: 'fab fa-linkedin-in',
                 text: '',
             },         
+        ],
+        navLinks: [
+            //Lista link nav principale che portano a sezioni specifiche
+            {
+                text: 'HOME',
+                href: '#'
+            },
+            {
+                text: 'SERVICES',
+                href: '#services'
+            },
+            {
+                text: 'ABOUT',
+                href: '#about'
+            },
+            {
+                text: 'PROJECTS',
+                href: '#projects'
+            },
+            {
+                text: 'RESULTS',
+                href: '#results'
+            }
         ]
     },
     methods: {
